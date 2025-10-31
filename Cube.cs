2be7a9925810c46597ab29ac046bd62131954ae2
mateirusu1 <matei.rusu1@student.usv.ro>
2025-10-31 13:49:26 +0200

    Add files via upload

---
+++ Cube.cs
@@ -0,0 +1,84 @@
+﻿//Rusu Sebastian 3133a;
+
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    
+    public class Cube
+    {
+        private bool newVisibility;
+
+        private const int DIM = 10;
+        public Cube()
+        {
+            newVisibility = false;
+        }
+
+        public void Hide()
+        {
+            newVisibility = false;
+        }
+
+        public void Show()
+        {
+            newVisibility = true;
+        }
+        public void ToggleVisibility()
+        {
+            newVisibility = !newVisibility;
+        }
+        public void DrawCube() // Functia care ne deseanaza cubul
+        {
+            if (newVisibility)
+            {
+                GL.Begin(PrimitiveType.Quads);
+
+                GL.Color3(Color.Silver);
+                GL.Vertex3(-1.0f*DIM, -1.0f * DIM, -1.0f * DIM);
+                GL.Vertex3(-1.0f * DIM, 1.0f * DIM, -1.0f * DIM);
+                GL.Vertex3(1.0f * DIM, 1.0f * DIM, -1.0f * DIM);
+                GL.Vertex3(1.0f * DIM, -1.0f * DIM, -1.0f * DIM);
+
+                GL.Color3(Color.Silver);
+                GL.Vertex3(-1.0f * DIM, -1.0f * DIM, -1.0f * DIM);
+                GL.Vertex3(1.0f * DIM, -1.0f * DIM, -1.0f * DIM) ;
+                GL.Vertex3(1.0f * DIM, -1.0f * DIM, 1.0f * DIM);
+                GL.Vertex3(-1.0f * DIM, -1.0f * DIM, 1.0f * DIM);
+
+                GL.Color3(Color.Silver);
+
+                GL.Vertex3(-1.0f * DIM, -1.0f * DIM, -1.0f * DIM);
+                GL.Vertex3(-1.0f * DIM, -1.0f * DIM, 1.0f * DIM);
+                GL.Vertex3(-1.0f * DIM, 1.0f * DIM, 1.0f * DIM) ;
+                GL.Vertex3(-1.0f * DIM, 1.0f * DIM, -1.0f * DIM);
+
+                GL.Color3(Color.Silver);
+                GL.Vertex3(-1.0f * DIM, -1.0f * DIM, 1.0f * DIM);
+                GL.Vertex3(1.0f * DIM, -1.0f * DIM, 1.0f * DIM);
+                GL.Vertex3(1.0f * DIM, 1.0f * DIM, 1.0f * DIM);
+                GL.Vertex3(-1.0f * DIM, 1.0f * DIM, 1.0f * DIM);
+
+                GL.Color3(Color.Silver);
+                GL.Vertex3(-1.0f * DIM, 1.0f * DIM, -1.0f * DIM);
+                GL.Vertex3(-1.0f * DIM, 1.0f * DIM, 1.0f * DIM);
+                GL.Vertex3(1.0f * DIM, 1.0f * DIM, 1.0f * DIM);
+                GL.Vertex3(1.0f * DIM, 1.0f * DIM, -1.0f * DIM);
+
+                GL.Color3(Color.Silver);
+                GL.Vertex3(1.0f * DIM, -1.0f * DIM, -1.0f * DIM);
+                GL.Vertex3(1.0f * DIM, 1.0f * DIM, -1.0f * DIM);
+                GL.Vertex3(1.0f * DIM, 1.0f * DIM, 1.0f * DIM);
+                GL.Vertex3(1.0f * DIM, -1.0f * DIM, 1.0f * DIM);
+
+                GL.End();
+            }   
+        }
+    }
+}
---
+++ CubeDown.cs
@@ -0,0 +1,100 @@
+﻿//Rusu Sebastian 3133a;
+
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class CubeDown
+    {
+        private bool visibility;
+        private bool isGravityBound;
+        private Color colour;
+        private List<Vector3> coordList;
+        private Randomizer rando;
+
+        private const int GRAVITY_OFFSET = 2;
+        public CubeDown()
+        {
+            rando = new Randomizer();
+
+            isGravityBound = true;
+            visibility = true;
+            colour = rando.RandomColor();
+
+            coordList = new List<Vector3>();
+            int size_offset = rando.RandomInt(15);
+            int height_offset = rando.RandomInt(35, 62);
+            int position_offset = rando.RandomInt(5, 24);
+
+            coordList.Add(new Vector3(0 * size_offset, 0 * size_offset + height_offset, 1 * size_offset + position_offset));
+            coordList.Add(new Vector3(0 * size_offset, 0 * size_offset + height_offset, 0 * size_offset + position_offset));
+            coordList.Add(new Vector3(1 * size_offset, 0 * size_offset + height_offset, 1 * size_offset + position_offset));
+            coordList.Add(new Vector3(1 * size_offset, 0 * size_offset + height_offset, 0 * size_offset + position_offset));
+            coordList.Add(new Vector3(1 * size_offset, 1 * size_offset + height_offset, 1 * size_offset + position_offset));
+            coordList.Add(new Vector3(1 * size_offset, 1 * size_offset + height_offset, 0 * size_offset + position_offset));
+            coordList.Add(new Vector3(0 * size_offset, 1 * size_offset + height_offset, 1 * size_offset + position_offset));
+            coordList.Add(new Vector3(0 * size_offset, 1 * size_offset + height_offset, 0 * size_offset + position_offset));
+            coordList.Add(new Vector3(0 * size_offset, 0 * size_offset + height_offset, 1 * size_offset + position_offset));
+            coordList.Add(new Vector3(0 * size_offset, 0 * size_offset + height_offset, 0 * size_offset + position_offset));
+        }
+
+        // deseneaza cubul
+        public void Draw()
+        {
+            if (visibility)
+            {
+                GL.Color3(colour);
+                GL.Begin(PrimitiveType.QuadStrip);
+                foreach (Vector3 v in coordList)
+                {
+                    GL.Vertex3(v);
+                }
+                GL.End();
+            }
+        }
+
+        // actualizeaza pozitia cubului
+        public void UpdatePosition()
+        {
+            if(visibility && isGravityBound && !GroundCollisonDetected())
+            {
+                for (int i = 0; i < coordList.Count; i++)
+                    coordList[i] = new Vector3(coordList[i].X, coordList[i].Y - GRAVITY_OFFSET, coordList[i].Z);
+            }
+        }
+
+        //verifica coliziunea cu solul
+        public bool GroundCollisonDetected()
+        {
+            foreach(Vector3 v in coordList)
+            { if (v.Y <= 0)
+                    return true;
+            }
+            return false;
+        }
+        public void ToggleVisibility()
+        {
+            visibility = !visibility;
+        }
+        public void ToggleGravity()
+        {
+            isGravityBound = !isGravityBound;
+        }
+        public void SetGravity()
+        {
+            isGravityBound = true;
+        }
+        public void UnsetGravity()
+        {
+            isGravityBound = false;
+        }
+
+    }
+}
